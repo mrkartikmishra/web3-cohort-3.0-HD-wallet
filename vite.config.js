861315f91b1b18618eed428212a3c91092ada084
Kartik Mishra <kartikmishra.km44@gmail.com>
2024-08-24 17:00:54 +0530

    build error resolved

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,10 @@ import wasm from "vite-plugin-wasm";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills(), wasm()],
+  build: {
+    target: "esnext", // Use 'esnext' to ensure support for the latest JavaScript features
+  },
+  esbuild: {
+    target: "esnext", // Ensure that esbuild supports top-level await
+  },
 });
